2b8fca5afafb87a15030dc5e3f9643fc919e8d84
kirank6 <git@github.com>
2021-01-22 18:47:08 -0600

    submit form

--- pages/forms.py
+++ pages/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class PagesForm(forms.Form):
     item_name = forms.CharField(label = 'Item Name', max_length = 120)
-    zip_code = forms.DecimalField(label = 'Zip Code')
+    zip_code = forms.IntegerField(label = 'Zip Code')
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import PagesForm
+import subprocess 
 
 # Create your views here.
 def homePageView(request):
@@ -10,8 +11,15 @@ def homePageView(request):
             note = "Thank you! Your %s is  \
             loading!" %(filled_form.cleaned_data['item_name'],)
             new_form = PagesForm()
+            # script_function(filled_form)
             return render(request, 'pages/home.html',{'pagesform':new_form,'note':note})
     
     else:
         form = PagesForm()
         return render(request,'pages/home.html',{'pagesform':form})    
+    
+# def script_function(post_from_form):
+#     print(post_from_form)
+#     return subprocess.check_call(['C:/Users/Ursa Major/Documents/Data_Science/Projects  \
+#                                   /SharpestMinds/Flipp_programs/webpage_backend_use.py'], post_from_form)
+    
