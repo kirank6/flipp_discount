1979a6ad7d0b8b199e2c22ddad2ed9bc90bfae52
Nabila <nabila.abraham@gmail.com>
2021-01-23 20:09:45 -0500

    connecting backend to frontend; basic website working

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homePageView
+from .views import homePageView, processView
 
 urlpatterns = [
-    path('', homePageView, name='home'),    
+    path('', homePageView, name='home'),
+    path('process', processView, name='process')
 ]   
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import PagesForm
 import subprocess 
+#from .process_item import 
+
+def get_price(item):
+    price = 12.99
+    url = "https://m.media-amazon.com/images/I/51DMel2czfL._AC_SX425_.jpg"
+    return price, url
 
 # Create your views here.
 def homePageView(request):
@@ -17,7 +23,17 @@ def homePageView(request):
     else:
         form = PagesForm()
         return render(request,'pages/home.html',{'pagesform':form})    
-    
+
+def processView(request):
+    context={}
+    item = request.POST.get('item_name', "you didnt input text")
+    price, imgurl = get_price(item)
+    context['price'] = price
+    context['item'] = item
+    context['img'] = imgurl
+
+    return render(request, 'pages/process.html', context)   
+
 # def script_function(post_from_form):
 #     print(post_from_form)
 #     return subprocess.check_call(['C:/Users/Ursa Major/Documents/Data_Science/Projects  \
