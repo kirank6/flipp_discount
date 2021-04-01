3fa37c292442df5999f85c922698cf8756478995
kirank6 <khanalkiran2007@gmail.com>
2021-03-31 23:48:10 -0500

    web background update

--- pages/forms.py
+++ pages/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class PagesForm(forms.Form):
-    item_name = forms.CharField(label = 'Item Name', max_length = 120)
-    zip_code = forms.CharField(label = 'Zip Code', max_length= 6)
+    item_name = forms.CharField(label = 'ITEM NAME', max_length = 120)
+    zip_code = forms.CharField(label = 'ZIP CODE', max_length= 6)
