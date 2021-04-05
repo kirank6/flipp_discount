801fd9a7c4377a8a8642313d1b6a3ed540065111
kirank6 <khanalkiran2007@gmail.com>
2021-04-04 20:50:22 -0500

    css update

--- flipp_discount/settings.py
+++ flipp_discount/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = os.getenv("DEBUG","False")=="True"
+DEBUG = True
+#DEBUG = os.getenv("DEBUG","False")=="True"
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "root")
 STATICFILES_DIRS = (
                    os.path.join(BASE_DIR, "static"),
                     )
